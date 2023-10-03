7f247ea9fdcf2d04fa35256e5b672fb683ed0ec9
Ulugbek <ulug1bek@gmail.com>
2023-10-03 17:52:56 +0500

    optimizatsiya

--- id2.js
+++ id2.js
@@ -1,5 +1,4 @@
-const NumberOfSeries = +prompt("nechta serial ko'rdingiz", "");
-
+var NumberOfSeries = +prompt("nechta serial ko'rdingiz", "");
 console.log(typeof NumberOfSeries);
 
 seriesDB = {
@@ -10,11 +9,22 @@ seriesDB = {
   private: false,
 };
 
-const a = prompt("Oxirgi ko'rgan serialingiz", ""),
-  b = +prompt("Nechi baho berasiz", ""),
-  c = prompt("Oxirgi ko'rgan serialingiz", ""),
-  d = +prompt("Nechi baho berasiz", "");
-seriesDB.series[a] = b;
-seriesDB.series[c] = d;
+for (let i = 0; i < 2; i++) {
+  const a = prompt("Oxirgi ko'rgan serialingiz", ""),
+    b = +prompt("Nechi baho berasiz", "");
 
+  if (a != "" && b != null && a != null && b != "") {
+    seriesDB.series[a] = b;
+  } else {
+    i--;
+  }
+}
+
+if (seriesDB.count == 5) {
+  console.log("Kam serial ko'ribsiz");
+} else if (seriesDB.count > 5 && seriesDB.count < 10) {
+  console.log("classic");
+} else {
+  console.log("serialchi zvezda ekansiz");
+}
 console.log(seriesDB);
